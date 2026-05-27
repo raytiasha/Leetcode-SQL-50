569b1ca58a5b046072bf93a7b7ae0cb2449cf067
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:53:53 +0530

    Update 1757. Recyclable and Low Fat Products.sql

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -4,6 +4,7 @@
 --- 2. recyclable = 'Y' -> This condition ensures that only recyclable products are selected.
 --- The AND operator is used so that only products satisfying both conditions are returned.
   
+# Write your MySQL query statement below
 SELECT
     product_id
 FROM
