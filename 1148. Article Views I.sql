3390a437cbc1a7ad51569395df6bed9d247b41dd
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:51:03 +0530

    Update 1148. Article Views I.sql

--- 1148. Article Views I.sql
+++ 1148. Article Views I.sql
@@ -1,1 +1,11 @@
+--- The objective of this query is to find all authors who viewed at least one of their own articles.
+--- DISTINCT -> removes duplicate author IDs.
+--- AS id -> renames the output column to id as required.
+--- WHERE author_id = viewer_id -> This condition selects only those rows where the author and viewer are the same person.
+--- The result are sorted in ascending order of author_id.
 
+# Write your MySQL query statement below
+SELECT distinct(author_id) as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id
