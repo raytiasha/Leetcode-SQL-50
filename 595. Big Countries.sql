3a5eca58f13e82944e9a664004cb5458517401ec
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:21:54 +0530

    Create 595. Big Countries.sql

---
+++ 595. Big Countries.sql
@@ -0,0 +1,12 @@
+--- This query retrieves the name, population, and area columns from the world table.
+--- area >= 3000000 -> This condition selects countries whose area is at least 3 million square kilometers.
+--- population >= 25000000 -> This condition selects countries whose population is at least 25 million.
+--- The OR operator ensures that a country is included if it satisfies either of the conditions.
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000 OR population >= 25000000
+;
