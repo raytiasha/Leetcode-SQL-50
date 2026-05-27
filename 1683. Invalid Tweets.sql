39b29784902509c6c4dcf4fe9646fff15c0a2eb5
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:26:13 +0530

    Update 1683. Invalid Tweets.sql

--- 1683. Invalid Tweets.sql
+++ 1683. Invalid Tweets.sql
@@ -1,1 +1,8 @@
+--- This query retrieves the tweet_id from the Tweets table for tweets whose content length is greater than 15 characters.
+--- The LENGTH() function is used to calculate the number of characters present in the content column.
+--- LENGTH(content) > 15 -> This condition filters tweets where the content contains more than 15 characters.
+--- Only tweets satisfying this condition are included in the result.
 
+select tweet_id
+from Tweets
+where length(content) >15
