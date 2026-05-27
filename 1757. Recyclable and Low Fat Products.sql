dc70799a989c088ca766da0f5377ec16dad95fa0
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:03:01 +0530

    Create 1757. Recyclable and Low Fat Products.sql

---
+++ 1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,12 @@
+--- SELECT statement to retrieve the product_id column from the Products table.
+--- WHERE clause filters the records based on two conditions:
+--- 1. low_fats = 'Y' -> This condition ensures that only low-fat products are selected.
+--- 2. recyclable = 'Y' -> This condition ensures that only recyclable products are selected.
+--- The AND operator is used so that only products satisfying both conditions are returned.
+  
+SELECT
+    product_id
+FROM
+    Products
+WHERE
+    low_fats = 'Y' AND recyclable = 'Y'
