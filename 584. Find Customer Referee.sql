feffe22f252e534fef69d7a8fae6e87088dc3ad5
Tiasha Ray <inboxtiasha@gmail.com>
2026-05-27 19:15:59 +0530

    Create 584. Find Customer Referee.sql

---
+++ 584. Find Customer Referee.sql
@@ -0,0 +1,6 @@
+--- This query retrieves customer names from the customer table while excluding customers referred by customer 2.
+--- 1. referee_id <> 2 -> This condition selects customers whose referee ID is not equal to 2.
+--- 2. referee_id IS NULL -> This condition includes customers who do not have any referee assigned.
+--- The OR operator combines both conditions so the query returns.
+  
+SELECT name FROM customer WHERE referee_id <> 2 OR referee_id IS NULL;
